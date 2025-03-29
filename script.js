880be93f7f946e8a34ee2716e694347b269a9d7e
PigeonPlayGames <158285083+PigeonPlayGames@users.noreply.github.com>
2025-03-29 15:24:41 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtkOSmD4meTdLdWbOfW53rM75lnYreSZo",
+  authDomain: "up-to-battle.firebaseapp.com",
+  projectId: "up-to-battle",
+  storageBucket: "up-to-battle.firebasestorage.app",
+  messagingSenderId: "328069667156",
+  appId: "1:328069667156:web:5f36cb5ee1a898b17310c1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
