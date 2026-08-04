30b83feccc6bed0d0bda38de5c7f32f3a3c6e414
PigeonPlayGames <158285083+PigeonPlayGames@users.noreply.github.com>
2026-08-04 15:48:11 +0100

    Refactor Firebase configuration and initialization
    Updated Firebase initialization to use Firebase SDK methods.

--- Tribal2/js/firebase-config.js
+++ Tribal2/js/firebase-config.js
@@ -1,8 +1,4 @@
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBCpMKnBIS30boGuPGkQoYEphy_JWQSdqY",
   authDomain: "tribalwars-2.firebaseapp.com",
@@ -11,9 +7,9 @@ const firebaseConfig = {
   storageBucket: "tribalwars-2.firebasestorage.app",
   messagingSenderId: "649122676235",
   appId: "1:649122676235:web:1e5e61df4eaf141f6e510e",
-  measurementId: "G-PV2HTW6T9J"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
